05754fd0b5841922a4ff70b8a5628eda0e695b7e
Brian Seward <blacktiger55@hotmail.com>
2018-05-07 14:31:24 -0600

    Afternoon_Update

--- app/playercontroller.js
+++ app/playercontroller.js
@@ -1,24 +1,41 @@
 function PlayerController() {
     var playersService = new PlayersService(drawPlayers);
 
-    // this.getPlayer = function getPlayer(e) {
-    //     e.preventDefault();
-    //     var player = e.target.player.value;
-    //     playersService.loadPlayersData(playersData).then(drawPlayers);
-    // }
-
     //SEARCH
 
     this.getPlayer = function getPlayer(e){
         e.preventDefault()
         var query = e.target.player.value
-        var results = playersService.search(query)
+        var results = playersService.getPlayer(query)
         drawPlayers(results)
         };
+        
+    //LOADS ALL PLAYERS
 
-    //LOADS MY PERSONAL TEAM
+    function drawPlayers(players) {
+        var template = '<h1>Results:</h1>'
+        console.log(players)
+        for (let i = 0; i < players.length; i++) {
+            var player = players[i];
+            template += `
+    <div>
+            <ul>
+        <li><img src="${player.photo}" alt=""></li>
+        <li>Name: ${player.fullname}</li>
+        <li>Team: ${player.pro_team}</li>
+        <li>Position: ${player.position}</li>
+        <li><button onclick="app.controllers.playerCtrl.addToTeam(${player.id})">Add</button></li>
+    </ul>
+    </div>
+        `
+        }
+        document.getElementById("results").innerHTML = template;
+    }
+
+        //LOADS MY PERSONAL TEAM
 
         function drawMyTeam(myPlayers) {
+        debugger
             var template = '<h1>My Team</h1>'
             for (let i = 0; i < myPlayers.length; i++) {
                 var myPlayer = myPlayers[i];
@@ -29,39 +46,21 @@ function PlayerController() {
         <li>Name: ${myPlayer.fullname}</li>
         <li>Team: ${myPlayer.pro_team}</li>
         <li>Position: ${myPlayer.position}</li>
-        <li><button onsubmit="app.controllers.playerCtrl.removeFromTeam(${myPlayer.id})">Remove</button></li>
+        <li><button onclick="app.controllers.playerCtrl.removeFromTeam(${myPlayer.id})">Remove</button></li>
         </ul>
         </div>
             `
             }
             document.getElementById("myteam").innerHTML = template;
-        }
         
-    //LOADS ALL PLAYERS
-
-    function drawPlayers(players) {
-        var template = '<h1>Results:</h1>'
-        for (let i = 0; i < players.length; i++) {
-            var player = players[i];
-            template += `
-    <div>
-            <ul>
-        <li><img src="${player.photo}" alt=""></li>
-        <li>Name: ${player.fullname}</li>
-        <li>Team: ${player.pro_team}</li>
-        <li>Position: ${player.position}</li>
-        <li><button onsubmit="app.controllers.playerCtrl.addToTeam(${player.id})">Add</button></li>
-    </ul>
-    </div>
-        `
         }
-        document.getElementById("results").innerHTML = template;
-    }
 
  
 //ADD TO MY ROSTER
 
+
 this.addToTeam = function addToTeam(id){
+    console.log(id)
     playersService.addToTeam(id, drawMyTeam)
   }
 
--- app/playerservice.js
+++ app/playerservice.js
@@ -1,64 +1,101 @@
-function PlayersService(callback){
+function PlayersService(callback) {
   var playersData = [];
   var myTeam = [];
 
-   function loadPlayersData(){
-     //check if the player already has a copy of the NFL playersData
-     var localData = localStorage.getItem('playersData');
+  function loadPlayersData() {
+    //check if the player already has a copy of the NFL playersData
+    var localData = localStorage.getItem('playersData');
     //if they do, pull from there
-     if(localData){
-         playersData = JSON.parse(localData);
-         //return will short-circuit the loadPlayersData function
-         //this will prevent the code below from ever executing
-           console.log(playersData) 
-           return // callback (playersData)
-     }
-     //if not go get that data
-     var url = "https://bcw-getter.herokuapp.com/?url=";
-     var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
-     var apiUrl = url + encodeURIComponent(endpointUri);
+    if (localData) {
+      playersData = JSON.parse(localData);
+      //return will short-circuit the loadPlayersData function
+      //this will prevent the code below from ever executing
+      console.log(playersData)
+      return // callback (playersData)
+    }
+    //if not go get that data
+    var url = "https://bcw-getter.herokuapp.com/?url=";
+    var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
+    var apiUrl = url + encodeURIComponent(endpointUri);
 
-       $.getJSON(apiUrl, function(data){
-         playersData = data.body.players;
-         console.log('Player Data Ready')
-         console.log('Writing Player Data to localStorage')
-         localStorage.setItem('playersData', JSON.stringify(playersData))
-         console.log('Finished Writing Player Data to localStorage')
-        //  callback(playersData)
-       });
-   }   
+    $.getJSON(apiUrl, function (data) {
+      playersData = data.body.players;
+      console.log('Player Data Ready')
+      console.log('Writing Player Data to localStorage')
+      localStorage.setItem('playersData', JSON.stringify(playersData))
+      console.log('Finished Writing Player Data to localStorage')
+      //  callback(playersData)
+    });
+  }
+  loadPlayersData();
 
+  //SEARCH
 
-   //SEARCH
-
-   this.search = function search(query){
+  this.getPlayer = function getPlayer(query) {
     var x = query.toLowerCase()
     var filteredResults = playersData.filter(function (player) {
-        return player.fullname.toLowerCase().includes(x) || player.pro_team.toLowerCase().includes(x) || player.position.toLowerCase().includes(x)
+      return player.fullname.toLowerCase().includes(x) || player.pro_team.toLowerCase().includes(x) || player.position.toLowerCase().includes(x)
     })
     return filteredResults
   }
-    
-//ADD TO MY ROSTER
 
-   this.addToTeam = function addToTeam(addPlayerId, cb){
-     var addPlayer = playersData.find(function(player){
-        return player.id == addPlayerId
-      })
-              myTeam.push(addPlayer)
-               cb(myTeam);
-            };
+  //ADD TO MY ROSTER
 
-//REMOVE FROM MY ROSTER
+  this.addToTeam = function addToTeam(addPlayerId, cb){
+    return playersData.filter(function (player)){
+      if (player.id = playerID){
+        if (!comparePlayer(player)){
+          if (myTeam.length <= 10){
+            if (!comparePosition(player)){
+              if (myTeam.push(addPlayer)){
+                return cb(myTeam);
+              }
+            }
+          }
+        }
+      }
+            }
+  }
 
-    this.removeFromTeam = function removeFromTeam(removePlayerId, cb) {
 
-      var removePlayer = myTeam.indexOf(removePlayerId)
-        myTeam.splice(removePlayer, 1)
-        cb(myTeam)
+  function comparePlayer(player) {
+    for (var i = 0; i < myTeam.length; i++) {
+      var element = myTeam[i];
+      if (player.id == element.id) {
+        return true
+      }
+      {
+        {
+        } return false {
+        }
+      }
     }
+  }
 
-loadPlayersData(); 
+  function comparePosition(position) {
+    for (var i = 0; i < myTeam.length; i++) {
+      var element = myTeam[i];
+      if (position.id == element.id) {
+        return true
+      }
+      {
+        {
+        } return false {
+
+
+  //REMOVE FROM MY ROSTER
+
+  this.removeFromTeam = function removeFromTeam(removePlayerId, cb) {
+    var removePlayer = myTeam.find(function (player) {
+      return player.id == removePlayerId
+    })
+
+    var index = myTeam.indexOf(removePlayer)
+    myTeam.splice(index, 1)
+    cb(myTeam)
+  };
+
+
 }
 
 // Need to create a for loop with if true statement to prevent adding same 
